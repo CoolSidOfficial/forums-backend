a1550d51c737ea9da4069ed1d1aa13eb5bbc70b6
CoolSid <hackersiddhant1@gmail.com>
2026-03-25 12:38:49 +0530

    updated cors policy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app=express()
 const PORT=process.env.PORT || 4000
 app.use(cookieParser());
 app.use(cors({
-origin: "https://forums-self.vercel.app",
+origin: ["https://forums-self.vercel.app","http://localhost:3000"],
 
   credentials: true
 }));
